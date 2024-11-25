d31f6e1275ae398df35819f2dcf47cab99ab531d
TalesLimaOliveira <104475181+TalesLimaOliveira@users.noreply.github.com>
2024-11-25 03:40:14 -0300

    Initial commit

--- Ex4_18-11-2/src/draw.cpp
+++ Ex4_18-11-2/src/draw.cpp
@@ -70,7 +70,7 @@ void display() {
 
             // Draw instance number inside the house
             glColor3f(0.0f, 0.0f, 0.0f);
-            glRasterPos2f(-0.02f, -0.05f);
+            glRasterPos2f(-0.01f, -0.051f);
             std::string instanceNumber = std::to_string(i);
             drawText(GLUT_BITMAP_TIMES_ROMAN_24, instanceNumber.c_str());
 
